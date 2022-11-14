26d3f77d1a5fce861c358b009ef7f85ac2d16501
Santo Khan <santokhan1999@gmail.com>
2022-11-14 13:06:24 +0600

    First Commit

---
+++ wp-content/themes/nifty/framework/js/framework_misc9b2d.js
@@ -0,0 +1,24 @@
+'use strict';
+
+<<<<<<< .mine
+/*window.onunload = function(){};
+||||||| .r104978
+window.onunload = function(){};
+=======
+/*
+
+window.onunload = function(){};
+>>>>>>> .r105684
+
+window.addEventListener( "pageshow", function( evt ){
+        if( evt.persisted ){
+        setTimeout( function(){
+            window.location.reload();
+        }, 10 );
+    }
+<<<<<<< .mine
+}, false);*/||||||| .r104978
+}, false);=======
+}, false );
+
+*/>>>>>>> .r105684
